19e8207fdb9015d1d54848dfa96b1a942e95c975
Vito Gentile <gentilevito1076@gmail.com>
2021-01-28 13:21:12 +0100

    File Preparati  + link + script OK

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/*TRACCIA
+Proseguendo l'esercitazione (snackone) in classe, creare una todolist dove l'utente potrà:
+scrivere in un input
+premendo invio nell'input oppure cliccando sul submit, salvare nella lista la nuova nota
+visualizzare tutte le note nel box sottostante
+cancellare la nota, cliccando sulla X corrispondente
+Bonus:
+lavorate lato CSS, per rendere la todolist più carina seguendo la vostra creatività.
+Vi lascio un esempio di todolist.*/
+
+
+var app = new Vue({
+ el: '#root',
+ data: {
+   text: 'testo'
+ }
+});
+Vue.config.devtools = true;
